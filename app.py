0ca7d899413bb5ddc42a4d9c5d91342b570f6022
Bryan Gomez <bgomez718@gmail.com>
2024-02-20 08:41:37 -0500

    Reactive Agent with Streamlit

--- app.py
+++ app.py
@@ -1,99 +1,42 @@
-import streamlit as st 
-from langchain.llms import OpenAI
-from langchain_community.chat_models import ChatOpenAI
-from langchain.chains import LLMChain
-from langchain.tools.render import render_text_description
-from langchain.prompts import PromptTemplate
+import streamlit as st
 from dotenv import load_dotenv
-from langchain.agents import AgentExecutor
-from langchain_core.tools import tool, Tool
-from langchain.agents.format_scratchpad import format_log_to_str
-from langchain.agents.output_parsers import ReActSingleInputOutputParser
 from typing import Union
-from langchain_core.agents import AgentFinish, AgentAction
+
+from langchain import hub 
+from langchain.agents import AgentExecutor, create_react_agent
+from langchain_openai import OpenAI 
+from langchain.tools import tool
+
 
 import os
 
 load_dotenv()
 
+openai_api_key = os.getenv("OPENAI_API_KEY")
+
+print(openai_api_key)
 
 @tool
-def get_text_length(text: str) -> int:
-    """Retruns the length of a text by characters"""
-    return len(text)
+def get_word_length(word: str) -> int:
+    """Returns the length of a word based on the amount of characters on the target word."""
+    return len(word)
 
-
-def find_tool_by_name(tools: list[Tool], tool_name: str) -> Tool:
-    for tool in tools:
-        if tool.name == tool_name:
-            return tool
-        raise ValueError("Tool with name {tool_name} not found}")
 
 def generate_response(input_text):
-    tools = [get_text_length]
+    tools=[get_word_length]
+    prompt = hub.pull("hwchase17/react")
+    print(prompt)
+    llm = OpenAI()
+    agent = create_react_agent(llm,tools,prompt)
 
-    template = """ 
-    Answer the following questions as best you can. You have access to the following tools:
-
-    {tools}
-
-    Use the following format:
-
-    Question: the input question you must answer
-    Thought: you should always think about what to do
-    Action: the action to take, should be one of [{tool_names}]
-    Action Input: the input to the action
-    Observation: the result of the action
-    ... (this Thought/Action/Action Input/Observation can repeat N times)
-    Thought: I now know the final answer
-    Final Answer: the final answer to the original input question
-
-    Begin!
-
-    Question: {input}
-    Thought: {agent_scratchpad}
-    """
-
-    prompt = PromptTemplate.from_template(template=template).partial(
-        tools=render_text_description(tools),
-        tool_names=", ".join([t.name for t in tools]),
-    )
-
-    llm = ChatOpenAI(temperature=0, stop=["\nObservation"])
-    intermediate_steps = []
-    agent = (
-        {
-            "input": lambda x: x["input"],
-            "agent_scratchpad": lambda x: format_log_to_str(x["agent_scratchpad"]),
-        }
-        | prompt
-        | llm
-        | ReActSingleInputOutputParser()
-    )
-    agent_step: Union[AgentAction, AgentFinish] = agent.invoke(
-        {"input": input_text,
-         "agent_scratchpad": intermediate_steps,
-         }
-    )
-    print(agent_step)
-
-    if isinstance(agent_step, AgentAction):
-        tool_name = agent_step.tool
-        tool_to_use = find_tool_by_name(tools, tool_name)
-        tool_input = agent_step.tool_input
-
-        observation = tool_to_use.func(str(tool_input))
-        intermediate_steps.append(agent_step, str(observation))
-
-        print(f"{observation}")
-  
-    
-
+    agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True,handle_parsing_errors=True)
+    res = agent_executor.invoke({"input": input_text})
+    return res.get("output")
 
 st.title("QUICK START APP🗣️")
-with st.form('my_form', clear_on_submit=True):
-    text = st.text_input('Enter Text:')
-    submitted = st.form_submit_button('Submit')
+with st.form("my_form", clear_on_submit=True):
+    text = st.text_input("Enter Text:")
+    submitted = st.form_submit_button("Submit")
     if submitted:
-        response = generate_response(text)
-        st.write("Final Answer: " + response)
+        response = generate_response("where is dad?")
+        st.write(response)
