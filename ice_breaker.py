59c4893d0a89a850c7568bb5bc32c7540c70468c
Teccon1998 <alexandercrw@gmail.com>
2024-02-22 14:55:05 -0500

    Added files as base of project

---
+++ ice_breaker.py
@@ -0,0 +1,43 @@
+import os
+from dotenv import load_dotenv
+from langchain.chains import LLMChain
+from langchain_openai import ChatOpenAI
+from langchain_core.prompts import PromptTemplate
+from agents.linkedin_lookup_agent import lookup as linkedin_lookup_agent
+from third_party.linkedin import scrape_linkedin_profile
+
+
+def icebreaker(name: str) -> str:
+    print("temp")
+    linkedin_profile_url = linkedin_lookup_agent(name=name)
+    linkedin_data = scrape_linkedin_profile(linkedin_profile_url=linkedin_profile_url)
+    print(linkedin_data)
+
+    summary_template = """
+        given the following information {information} about a person I want you to create:
+        1. A short summary
+        2. Two short interesting facts about them
+        3. A long summary
+        4. 2-3 creative icebreakers based on the knowledge given.
+        """
+
+    summary_prompt_template = PromptTemplate(
+        input_variables=["information"], template=summary_template
+    )
+
+    openai_api_key = os.environ['OPENAI_API_KEY']
+
+    llm = ChatOpenAI(
+        temperature=0, model_name="gpt-3.5-turbo", openai_api_key=openai_api_key
+    )
+
+    chain = LLMChain(llm=llm, prompt=summary_prompt_template)
+    result = chain.invoke({"information": linkedin_data})
+    return result
+
+
+if __name__ == "__main__":
+    load_dotenv()
+    name = "Pradeep Atrey"
+    res = icebreaker(name=name)
+    print(res)
