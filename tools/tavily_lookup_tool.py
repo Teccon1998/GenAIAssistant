1a11732f375411a7076ba4038fd0f5cbbf71833d
Manfred Fong <mafong081102@gmail.com>
2024-02-29 21:33:39 -0500

    Updated the prompt on the Tavily Tool. It now only list all the online links associated with the query

--- tools/tavily_lookup_tool.py
+++ tools/tavily_lookup_tool.py
@@ -14,7 +14,7 @@ import os
 load_dotenv(find_dotenv())
 #This function uses the Tavily Search Engine to return all the social media accounts of a person
 def lookup(query:str)->str:
-    """Searches for a person from their name and reurns information from their various social media pages"""
+    """Searches for a person from their name and returns URL of their various social media pages"""
     llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0.7)
 
     #identify the tools
@@ -25,17 +25,19 @@ def lookup(query:str)->str:
     ]
 
     #get user prompt
-    instructions="""You are a search engine that given the full name of a person {name_of_person}, I want 
-                    you to get as much information as possible using  {name_of_person} various social media.
+    instructions="""You are a search engine that given the full name of a person {name_of_person} find all of the URL links of various online profiles
+                   assioated with the name {name_of_person},
+                   You must list all the URLs to assosiated with the full name: {name_of_person}
+                   various social media accounts as a bulleted list and Include the  URLs with the list. Your purpose is just to display URLs
+                   associated with {name_of_person}. Your purpose is not to determine who the person is. There might be multiple people with the same name. 
+                   Dont do anything except returning a bulleted list of URLs.
 
-                    Include the occupation of the person based on their linkedin profile {occupation_of_person},
-                    the education associated with the person {education_of_person},
-                    the professional work experience of the person based on their linkedin profile {experience_of_person},
-                    social Media links of the person {social_media_links_of_person}
 
-                    
-                    I want you to give me as much detailed information as possible on that individual based on the 
-                    social media of that person
+                    ex)
+                    Harrison Chase
+                    LinkedIn: https://www.linkedin.com/in/harrison-chase-961287118 
+                    Twitter: https://twitter.com/hwchase17/status/1695490295914545626
+                    ...
                  """
     
     base_prompt=hub.pull("langchain-ai/openai-functions-template")
@@ -53,18 +55,24 @@ def lookup(query:str)->str:
     return tavily_results
 
 @tool #Create Tavily Search Tool
-def tavilySearchTool(query:str)->str:
-    """Use to search information about people and create a summary in the format of the summary template"""
-    tavily_search_results=lookup(query)
+def tavilySearchTool(name:str)->str:
+    """Use to search for social media pages based on the full name of person  in the prompt"""
+    tavily_search_results=lookup(name)
 
     summary_template="""
-                      summerize this information {information}. 
-                      Make sure to list the following:
-                      1) Details on the work experience of the individual
-                      2) Details on the education of the individual
-                      3) Details on how I can contact the individual
-                      4) Details on the individual's various social media accounts
-                      5) Links to the individual's various social media accounts
+                      Start with letting the user know that you are listing out all the social media pages associated with the name: {name_of_person}
+                      Then list out all the social media pages and URLs assosiated with the full name: {name_of_person}
+                      You must list all the URLs to assosiated with the full name: {name_of_person}
+                      Underline the name. 
+
+
+                      ex)
+                      Harrison Chase
+                      LinkedIn: https://www.linkedin.com/in/harrison-chase-961287118 
+                      Twitter: https://twitter.com/hwchase17/status/1695490295914545626
+
+                      ... 
+                      These are all the profiles I can find associated with the name {name_of_person}
                      """
     summary_prompt_template=PromptTemplate(
         input_variables=["information"],template=summary_template
@@ -75,5 +83,12 @@ def tavilySearchTool(query:str)->str:
     )
 
     chain = LLMChain(llm=llm,prompt=summary_prompt_template,verbose=True)
-    result=chain.invoke({"information": tavily_search_results})
+    result=chain.invoke({"name_of_person": tavily_search_results})
+
+    #format the response. Replace new lines with a space
+    if "\n" in result:
+        result=result.replace("\n"," ")
+    else:
+        return result
+    
     return result
