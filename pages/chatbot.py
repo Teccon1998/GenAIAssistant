2e8d43fae9415186264635928d8fd080e6e98d2d
Manfred Fong <mafong081102@gmail.com>
2024-05-04 17:26:57 -0400

    implemented coreference memory to the chatbot

--- pages/chatbot.py
+++ pages/chatbot.py
@@ -1,23 +1,20 @@
-#This file will implement the chatbot feature
+# This file will implement the chatbot feature
 from langchain_openai import ChatOpenAI
 from langchain.memory import ConversationBufferMemory
 from langchain.schema import AIMessage, HumanMessage
 
-
-from dotenv import load_dotenv,find_dotenv 
+from dotenv import load_dotenv, find_dotenv 
 from langchain.agents import AgentExecutor, create_openai_tools_agent
 
-#LangChain Messages
+# LangChain Messages
 from langchain_core.chat_history import BaseChatMessageHistory
 from langchain_community.chat_message_histories import ChatMessageHistory
-import secrets # for generating random session ID
+import secrets  # for generating random session ID
 
-
-#Message Prompt Tools
+# Message Prompt Tools
 from langchain.prompts import (
     ChatPromptTemplate,
     MessagesPlaceholder, 
-    
 )
 
 from langchain_core.runnables.history import RunnableWithMessageHistory
@@ -27,22 +24,15 @@ import streamlit as st
 from streamlit_chat import message
 from tools.tavily_lookup_tool import tavilySearchTool
 from tools.FileManagementTool import FileTool
-from langchain import hub
 
-
-
-#load enviroment variables
+# Load environment variables
 load_dotenv(find_dotenv())
-#intialize chat model
+
+# Initialize chat model
 chat = ChatOpenAI(model="gpt-3.5-turbo", temperature=0)
-#initialize memory
-memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
 
-chat_session_token=secrets.token_hex(16)
-
-
-def generate_response(query:str):
-    
+# Function to generate response
+def generate_response(query:str, chat_session_token):
     file_tool = FileTool()
     tools = [tavilySearchTool, file_tool]
     
@@ -51,20 +41,16 @@ def generate_response(query:str):
         ("system", 
          "You are a helpful assistant. You may not need to use tools for every query - the user may just want to chat!",
          ),
-        #MessagesPlaceholder(variable_name="messages"),
-        #MessagesPlaceholder(variable_name="messages"),
         MessagesPlaceholder(variable_name="chat_history"), ("human", "{query}"),
         MessagesPlaceholder(variable_name="agent_scratchpad"),
     ])
     
-    #agent = create_react_agent(llm=llm, tools=tools, prompt=prompt,)
     agent = create_openai_tools_agent(chat, tools, prompt,)
 
     agent_executor = AgentExecutor(
         agent=agent, 
         tools=tools,
         verbose=True,
-        memory=memory
     )          
     agent_with_chat_history = RunnableWithMessageHistory(
         agent_executor,
@@ -72,53 +58,48 @@ def generate_response(query:str):
         input_messages_key="query",
         history_messages_key="chat_history",
     )
-    res = agent_with_chat_history.invoke({"query": query},config={"configurable": {"session_id": chat_session_token}},) 
+    res = agent_with_chat_history.invoke({"query": query}, config={"configurable": {"session_id": chat_session_token}})
+    print(f"#2: {chat_session_token}")  # For Debugging purposes
 
     return res.get("output")
 
-#get session histor function
-def get_session_history(session_id:str)-> BaseChatMessageHistory:
-    #add chat history
+# Get session history function
+def get_session_history(session_id:str) -> BaseChatMessageHistory:
     if session_id not in st.session_state:
-        st.session_state[session_id]=ChatMessageHistory()
+        st.session_state[session_id] = ChatMessageHistory()
         
     return st.session_state[session_id]
 
-
 #######################################################################################################
-#USER INTERFACE
+# USER INTERFACE
 st.header("AI Chat Assistant")
-#check for previouse user prompts.
+
+# Check for previous user prompts.
 if "user_prompt_history" not in st.session_state:
-    st.session_state["user_prompt_history"]=[]
+    st.session_state["user_prompt_history"] = []
 
-#check for chat answers
+# Check for chat answers
 if "chat_message_history" not in st.session_state:
-    st.session_state["chat_message_history"]=[]
-    
+    # Create Chat Session Token
+    st.session_state["chat_message_history"] = []
+    chat_session_token = secrets.token_hex(16)
+    st.session_state["chat_session_token"] = chat_session_token
+    print(f"#1: {chat_session_token}")  # For debugging purposes
 
-if prompt:= st.chat_input("Enter your message"):
+chat_session_token = st.session_state.get("chat_session_token")
+
+if prompt := st.chat_input("Enter your message"):
     with st.spinner("Generating Response..."):
-        response=generate_response(prompt)
-        #Variable that will be displayed to the user when
-        formated_response=(
-            f"{response}"
-        )
+        response = generate_response(prompt, chat_session_token)
+        formated_response = f"{response}"
 
         st.session_state["user_prompt_history"].append(prompt)
         st.session_state["chat_message_history"].append(formated_response)
-        #add context to memory
-        messages = [
-            HumanMessage(content=prompt),
-            AIMessage(content=formated_response)
-        ]
-        
-        st.session_state[chat_session_token].add_message(messages)
-        
+        st.session_state["chat_session_token"] = chat_session_token
 
-#if the streamlit session state is not empty. Output responses
+# if the Streamlit session state is not empty, output responses
 if st.session_state["chat_message_history"]:
     for response, user_query in zip(st.session_state["chat_message_history"], st.session_state["user_prompt_history"]):
-        #give each message widget a unique key not based off of output
-        message(user_query,is_user=True, key=secrets.token_hex(8))
+        # give each message widget a unique key not based off of output
+        message(user_query, is_user=True, key=secrets.token_hex(8))
         message(response, key=secrets.token_hex(8))
